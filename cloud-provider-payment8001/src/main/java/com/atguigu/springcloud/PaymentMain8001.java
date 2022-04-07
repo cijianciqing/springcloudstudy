8046526cbfdd367bc38b352636be3fb6a599da6e
cjcq <425430798@qq.com>
2022-04-07 14:45:20 +0800

    basic order&payment 完成 episode 14

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -7,16 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
-@RestController
 @SpringBootApplication
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
     }
 
-    @GetMapping("/testController")
-    public void test(){
-        log.info("***********com.atguigu.springcloud.PaymentMain8001.test");
-    }
 }
