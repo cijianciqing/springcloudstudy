f39539e883c7e2689b0bc5f748fee096a16ffb9e
cjcq <425430798@qq.com>
2022-04-07 10:33:02 +0800

    resolve bug

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -1,11 +1,22 @@
 package com.atguigu.springcloud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RestController
 @SpringBootApplication
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
+    }
+
+    @GetMapping("/testController")
+    public void test(){
+        log.info("***********com.atguigu.springcloud.PaymentMain8001.test");
     }
 }
