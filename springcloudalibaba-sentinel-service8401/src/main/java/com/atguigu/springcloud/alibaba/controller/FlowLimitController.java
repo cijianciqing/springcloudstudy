cb32a7f235b2ba529d053ac3e356ab6169d4f818
cjcq <425430798@qq.com>
2022-04-17 18:00:25 +0800

    sentinel 各种测试，直到 125 hot key

--- springcloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ springcloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @auther zzyy
  * @create 2020-02-24 16:26
@@ -16,8 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class FlowLimitController
 {
     @GetMapping("/testA")
-    public String testA()
-    {
+    public String testA() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2L);
+        log.info(Thread.currentThread().getName()+"\t"+"...testA");
         return "------testA";
     }
 
@@ -28,4 +31,18 @@ public class FlowLimitController
         return "------testB";
     }
 
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotHot",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "pp",required = false) String p1,
+                             @RequestParam(value = "bb",required = false) String bb)
+    {
+        log.info(Thread.currentThread().getName()+"\t"+"...testHotKey");
+        return "------testHotKey";
+    }
+
+    public String deal_testHotKey (String p1, String p2, BlockException exception)
+    {
+        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
+    }
 }
