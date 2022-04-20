1886fa21c147641d1693e7f10f9f2d8a6e9f5973
cjcq <425430798@qq.com>
2022-04-20 20:20:15 +0800

    解决小bug

--- seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/config/ApplicationContextConfig.java
+++ seata-order-service2001/src/main/java/com/atguigu/springcloud/alibaba/config/ApplicationContextConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
  * @auther zzyy
  * @create 2020-02-23 14:45
  */
-@Configuration
+//@Configuration
 public class ApplicationContextConfig
 {
     @Bean
