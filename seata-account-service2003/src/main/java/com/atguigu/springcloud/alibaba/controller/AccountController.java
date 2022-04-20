6023474353087cbe428e43bcb48c7d90f413d28d
cjcq <425430798@qq.com>
2022-04-20 18:40:29 +0800

    142-147.创建seata modules，并测试分布式事务

---
+++ seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/controller/AccountController.java
@@ -0,0 +1,29 @@
+package com.atguigu.springcloud.alibaba.controller;
+
+import com.atguigu.springcloud.alibaba.domain.CommonResult;
+import com.atguigu.springcloud.alibaba.service.AccountService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+
+@Slf4j
+@RestController
+public class AccountController {
+
+    @Resource
+    AccountService accountService;
+
+    /**
+     * 扣减账户余额
+     */
+    @RequestMapping("/account/decrease")
+    public CommonResult decrease(@RequestParam("userId") Long userId, @RequestParam("money") BigDecimal money){
+        log.info("收到 减少账户余额 请求");
+        accountService.decrease(userId,money);
+        return new CommonResult(200,"扣减账户余额成功！");
+    }
+}
